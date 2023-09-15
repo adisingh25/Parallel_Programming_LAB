67000e5900a2510c1bc7534e3c90d841f9051f5c
ADITYA SINGH <95960794+adisingh25@users.noreply.github.com>
2023-09-15 11:47:12 +0530

    Updated  2.c

--- test_codes/calculatingPI/2.c
+++ test_codes/calculatingPI/2.c
@@ -7,20 +7,20 @@ void main()
 {
 	int i;
 	double x,pi,sum[NUM_THREADS];
-step = 1.0/(double) num_steps;
+	step = 1.0/(double) num_steps;
 	#pragma omp parallel private(i,x)
 	{
 		int id = omp_get_thread_num();
-        double local_sum = 0.0;
+        	double local_sum = 0.0;
 		for(i = id,sum[id]=0.0;i<num_steps;i+=NUM_THREADS){
 			x = (i+0.5)*step;
 			local_sum+= 4.0/(1.0+x*x);
 		}
 
-        #pragma omp critical
-        {
-            sum[0] += local_sum;
-        }
+	        #pragma omp critical
+	        {
+	            sum[0] += local_sum;
+	        }
 	}
 	
 	pi = sum[0]/num_steps;
